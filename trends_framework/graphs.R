055af84414ed364437a9bbf736ffa07ff284f3cd
rogerioluizsi <rlcsf@cin.ufpe.br>
2023-02-16 21:25:57 -0300

    update

--- trends_framework/graphs.R
+++ trends_framework/graphs.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(envalysis)
 library(ggcorrplot)
+outputs<- read.csv("~/projects/panel/features_engineering/result_paperII.csv")
 outputs<- read.csv("~/projects/BRACIS21/result_paperII.csv")
 
 
@@ -72,7 +73,7 @@ labels = c(EDU_MAE = "Mother's education", EDU_PAI = "Father's education", RENDA
 #greelow =#d1f2b6
 #greehigh =#1d3d03
 
-classifier = "LogisticRegression"
+classifier = "RandomForestClassifier"
 df<-outputs%>%filter(clas==classifier)%>%
   group_by(name, year)%>%
   summarise(mua=mean(max), mem= (mean(mem)), std=mean(std), aabsa=mean(ale_0))  
@@ -87,8 +88,8 @@ if (classifier == "LogisticRegression"){
 
 
 #BoxPlot all features
-df%>% 
-  ggplot(aes(name, aabsa))+ geom_boxplot(outlier.shape = NA) +geom_jitter(aes(colour = year)) +
+plot1<- df%>% 
+  ggplot(aes(name, mua))+ geom_boxplot(outlier.shape = NA) +geom_jitter(aes(colour = year)) +
   scale_colour_gradient(low = low, high = high)+
   scale_x_discrete(labels=labels) + geom_hline(yintercept=0, linetype="dashed", color = "red")+
   coord_flip()+theme_publish()+
@@ -96,48 +97,53 @@ df%>%
         axis.text.y = element_text(angle = 0, hjust =1, color = 'gray15'),
         legend.position="right", 
         legend.title = element_text())+xlab("Features")+ylab("Max Uncentered ALE")+labs(colour="Years")
+ggsave(filename = "mua_lr_all-variables.png", plot = plot1, dpi = 800,width = 8, height = 6)
 
-df%>% 
-  ggplot(aes(name, std))+ geom_boxplot(outlier.shape = NA) +geom_jitter(aes(colour = year)) +
-  scale_colour_gradient(low = low, high = high)+
-  scale_x_discrete(labels=labels) + geom_hline(yintercept=0, linetype="dashed", color = "red")+
-  coord_flip()+theme_publish()+
-  theme(axis.text.x = element_text(angle = 0, hjust =1, color = 'gray15'),
-        axis.text.y = element_text(angle = 0, hjust =1, color = 'gray15'),
-        legend.position="right", 
-        legend.title = element_text())+xlab("Variables")+ylab("Max Uncentered ALE")+labs(colour="Years")
+#plot2<- df%>% 
+#  ggplot(aes(name, std))+ geom_boxplot(outlier.shape = NA) +geom_jitter(aes(colour = year)) +
+#  scale_colour_gradient(low = low, high = high)+
+#  scale_x_discrete(labels=labels) + geom_hline(yintercept=0, linetype="dashed", color = "red")+
+#  coord_flip()+theme_publish()+
+#  theme(axis.text.x = element_text(angle = 0, hjust =1, color = 'gray15'),
+#        axis.text.y = element_text(angle = 0, hjust =1, color = 'gray15'),
+#        legend.position="right", 
+#        legend.title = element_text())+xlab("Variables")+ylab("Max Uncentered ALE")+labs(colour="Years")
 
-
+#ggsave(filename = "smua_rf_all-variables.png", plot = plot2, dpi = 400, width = 9, height = 7)
 
 #Evolution of feature importance (MUA) of selected features set
-selected = c('NU_CIENCIA_NATUREZA','NU_CIENCIAS_HUMANAS','NU_LINGUAGENS_CODIGOS', 'NU_MATEMATICA', 'DIVERSIDADE', 'IN_FORM_DOCENTE' )
-ggplot(df%>%filter(name %in% selected)%>%
+selected = c('NU_CIENCIA_NATUREZA','NU_MATEMATICA','NU_LINGUAGENS_CODIGOS', 'NU_CIENCIAS_HUMANAS', 'DIVERSIDADE', 'IN_FORM_DOCENTE' )
+selected = c('NU_MATEMATICA', 'DIVERSIDADE', 'NU_CIENCIAS_HUMANAS')
+selected = c('NU_LINGUAGENS_CODIGOS', 'IN_FORM_DOCENTE')
+
+
+plot3<-ggplot(df%>%filter(name %in% selected)%>%
          select(mua, year, name), aes(x=as.character(year), y=mua, group=name, color=name))+
-  scale_color_viridis_d(labels = labels) + geom_point( size = 1)+ geom_line()#geom_smooth(method=lm, se = FALSE)+ 
-  guides(colour = guide_legend(override.aes = list(size = 2)))+theme_publish()+
+  scale_color_viridis_d(labels = labels) + geom_point( size = 5)+geom_line()+geom_smooth(method=lm, se = FALSE)+ 
+  guides(colour = guide_legend(override.aes = list(size = 5)))+theme_publish()+
   ylab("Max Uncentered ALE") +xlab("Years")+ ylim(-0.1, 0.2) + theme(legend.position="right", 
                                                                       legend.title = element_blank(), 
-                                                                      #text=element_text(size=12),
+                                                                      text=element_text(size=18),
                                                                       #legend.key=element_rect(fill=NA),
                                                                       axis.text.x = element_text(color = 'gray15'),
-                                                                      axis.text.y = element_text(color = 'gray15'))%>%ggplotly
-+
-  ggtitle(classifier)
- 
+                                                                      axis.text.y = element_text(color = 'gray15'))
+ggsave(filename = "some_var_2.png", plot = plot3, dpi = 800, width = 8, height = 6)
+
 
 ##CORRELATION
 corr<- df%>%group_by(name, year)%>%summarise(mua=mean(mua))%>%tidyr::pivot_wider(names_from = name, values_from = mua)%>%
   select(-year)
 
+
 corr <- round(cor(corr%>%select(selected)), 1)
-colnames(corr)<-c("Natural science faculty", "Humanities faculty", 'Languages faculty', "Math faculty",
+colnames(corr)<-c("Natural science faculty", "Math faculty",'Languages faculty',"Humanities faculty",
                  "Faculty work overload", "Faculty adequate training")
-rownames(corr)<-c("Natural science faculty", "Humanities faculty", 'Languages faculty', "Math faculty",
+rownames(corr)<-c("Natural science faculty", "Math faculty",'Languages faculty',"Humanities faculty",
                   "Faculty work overload", "Faculty adequate training")
-ggcorrplot(corr, hc.order = FALSE, type = "lower",
+plot4<-ggcorrplot(corr, hc.order = FALSE, type = "lower",
            lab = TRUE)
-
-
+plot4
+ggsave(filename = "correlation.png", plot = plot4, dpi = 400, width = 8, height = 6)
   
 p1<-df%>%filter(name %in% teacher_features)%>%
   select(name, mua,mem,aabsa,year)%>%mutate(grupo = "Teacher's Features")
@@ -151,13 +157,16 @@ histo<- rbind(p1,p2,p3,p4)
 
 
 
-histo%>%ggplot(aes(x=grupo, y =mua, color=grupo))+geom_boxplot()+geom_jitter()+
+plot5<-histo%>%ggplot(aes(x=grupo, y =mem, color=grupo))+geom_boxplot()+geom_jitter()+
   geom_hline(yintercept=0, linetype="dashed", color = "red")+
   scale_fill_viridis(discrete=TRUE) + scale_color_viridis(discrete=TRUE) + theme_publish()+
   xlab("")+ylab(" UAS (at average)")+theme(legend.title = element_blank(),
-                                                  #text=element_text(size=12),
+                                                  text=element_text(size=18),
                                                   axis.text.x = element_text(color = 'gray15'),
                                                   axis.text.y = element_text(color = 'gray15'))
+
+
+ggsave(filename = "histo.png", plot = plot5, dpi = 800, width = 8, height = 6)
                                                   #legend.text = element_text(size = 12))
 #+
  # ggtitle(classifier)
@@ -198,7 +207,7 @@ top_4%>%ggplot(aes(x=mem, color=grupo))+geom_density(alpha=0.6)+
   ggtitle(classifier)
 
 track_importance<-histo%>%group_by(year)%>%arrange(desc(aabsa))%>%do(head(., n=10))%>%
-  count(year, grupo)%>%group_by(year)%>%mutate(n = n/sum(n))
+  group_by(year, grupo)%>%mutate(n = n()/10)
 ggplot(data=track_importance, aes(x=as.character(year), group=grupo, fill=grupo)) +
   geom_density(adjust=0.2,position="fill") +
   scale_fill_viridis(discrete=TRUE) + scale_color_viridis(discrete=TRUE)+
@@ -213,7 +222,7 @@ aucs<-outputs%>%group_by(year, clas)%>%summarise(AUC = mean(auc), MAX_KS2 = mean
 aucs<- aucs%>%tidyr::gather(metric, value, MAX_KS2, AUC)
 aucs$clas<- if_else(aucs$clas=="LogisticRegression", "Logistic Regression", "Random Forest")
 #Performance of Models based on AUC
-ggplot(aucs, aes(as.character(year), value, shape=metric, linetype=clas, group=interaction(metric, clas)))+
+plot7<-ggplot(aucs, aes(as.character(year), value, shape=metric, linetype=clas, group=interaction(metric, clas)))+
   geom_point(size=3) +geom_line()+ylab("Evaluation Metrics")+ ylim(0.5, 1)+theme_publish()+
   theme(legend.title = element_blank(), legend.text=element_text())+ xlab(element_blank())+
   theme(legend.title = element_blank(),
@@ -223,6 +232,7 @@ ggplot(aucs, aes(as.character(year), value, shape=metric, linetype=clas, group=i
         axis.text.y = element_text(color = 'gray15'),
         legend.spacing.y = unit(1, 'cm'))
 
+ggsave(filename = "aucs.png", plot = plot7, dpi = 400, width = 8, height = 6)
 
 
 #t-testggplot(uspopage, aes(x = Year, y = Thousands, fill = AgeGroup)) +
